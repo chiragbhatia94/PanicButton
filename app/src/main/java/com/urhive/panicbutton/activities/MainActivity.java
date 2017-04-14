2da5f95696950760ece810123ea27c7ac58f60c3
Chirag Bhatia <chiragbhatia94@gmail.com>
2017-04-14 20:03:29 +0530

    Added floating layout

--- app/src/main/java/com/urhive/panicbutton/activities/MainActivity.java
+++ app/src/main/java/com/urhive/panicbutton/activities/MainActivity.java
@@ -1,10 +1,13 @@
 package com.urhive.panicbutton.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.google.firebase.auth.EmailAuthProvider;
@@ -12,6 +15,7 @@ import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 import com.urhive.panicbutton.R;
+import com.urhive.panicbutton.services.EmergencyButtonService;
 
 import java.util.Iterator;
 
@@ -82,8 +86,18 @@ public class MainActivity extends AppCompatBase {
     }
 
     public void startService(View view) {
+        Log.i(TAG, "startService: " + "clicked on startService button");
+
+        Intent intent = new Intent(MainActivity.this, EmergencyButtonService.class);
+        startService(intent);
     }
 
     public void stopService(View view) {
+        Log.i(TAG, "stopService: " + "clicked on stop service button");
+
+        Intent intent = new Intent(MainActivity.this, EmergencyButtonService.class);
+        stopService(intent);
+
+        Toast.makeText(this, R.string.panic_button_stopped, Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/urhive/panicbutton/services/EmergencyButtonService.java
@@ -0,0 +1,148 @@
+package com.urhive.panicbutton.services;
+
+import android.annotation.SuppressLint;
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import com.urhive.panicbutton.R;
+
+/**
+ * Created by Chirag Bhatia on 14-04-2017.
+ */
+
+public class EmergencyButtonService extends Service {
+
+    private static final String TAG = "EmergencyButtonService";
+    private static WindowManager windowManager;
+    private static WindowManager.LayoutParams params;
+    private boolean mHasDoubleClicked = false;
+    private long lastPressTime;
+    private Boolean _enable = true;
+    private Boolean _expanded = false;
+    private View view;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @SuppressLint("InflateParams")
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        view = LayoutInflater.from(this).inflate(R.layout.floating_layout, null);
+
+        params = new WindowManager.LayoutParams(WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                // WindowManager.LayoutParams.TYPE_PHONE,
+                WindowManager.LayoutParams.TYPE_SYSTEM_ERROR, WindowManager.LayoutParams
+                .FLAG_NOT_FOCUSABLE, PixelFormat.TRANSLUCENT);
+
+        params.gravity = Gravity.TOP | Gravity.START;
+        params.x = 0;
+        params.y = 100;
+
+        // for testing
+        windowManager.addView(view, params);
+
+        Toast.makeText(EmergencyButtonService.this, R.string.panic_button_activated, Toast
+                .LENGTH_SHORT).show();
+
+        try {
+            view.setOnTouchListener(new View.OnTouchListener() {
+                private WindowManager.LayoutParams paramsF = params;
+                private int initialX;
+                private int initialY;
+                private float initialTouchX;
+                private float initialTouchY;
+
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    switch (event.getAction()) {
+                        case MotionEvent.ACTION_DOWN:
+
+                            // Get current time in nano seconds.
+                            long pressTime = System.currentTimeMillis();
+
+                            // If double click...
+                            if (pressTime - lastPressTime <= 300) {
+                                // my code here
+                                //unregisterReceiver(mLockScreenStateReceiver);
+                                /*ServiceEmergencyButton.this.stopSelf();
+
+                                createNotification();*/
+                                mHasDoubleClicked = true;
+                            } else {
+                                // If not double click....
+                                mHasDoubleClicked = false;
+                            }
+                            lastPressTime = pressTime;
+                            initialX = paramsF.x;
+                            initialY = paramsF.y;
+                            initialTouchX = event.getRawX();
+                            initialTouchY = event.getRawY();
+                            break;
+                        case MotionEvent.ACTION_UP:
+                            break;
+                        case MotionEvent.ACTION_MOVE:
+                            paramsF.x = initialX + (int) (event.getRawX() - initialTouchX);
+                            paramsF.y = initialY + (int) (event.getRawY() - initialTouchY);
+                            windowManager.updateViewLayout(view, paramsF);
+                            break;
+                    }
+                    return false;
+                }
+            });
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (_expanded) {
+                    // set hide mode now
+                    setUnexpanded();
+                } else {
+                    // set show mode now
+                    setExpanded();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (view != null) windowManager.removeView(view);
+    }
+
+    private void setUnexpanded() {
+        // set hide mode now
+        _expanded = false;
+        //mainRL.setVisibility(View.GONE);
+        //floatingIV.setVisibility(View.VISIBLE);
+        Log.i(TAG, "onClick: this is hidden mode");
+    }
+
+    private void setExpanded() {
+        // set show mode now
+        //mainRL.setVisibility(View.VISIBLE);
+        //floatingIV.setVisibility(View.GONE);
+        _expanded = true;
+        Log.i(TAG, "onClick: this is shown mode");
+    }
+}
