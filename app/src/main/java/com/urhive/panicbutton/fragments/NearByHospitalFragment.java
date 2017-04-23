bbf32616bde7071060b587f86b2755e5a52db216
Chirag Bhatia <chiragbhatia94@gmail.com>
2017-04-23 21:40:27 +0530

    almost complete need to create input forms

--- app/src/main/java/com/urhive/panicbutton/fragments/NearByHospitalFragment.java
+++ app/src/main/java/com/urhive/panicbutton/fragments/NearByHospitalFragment.java
@@ -4,6 +4,7 @@ import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -28,12 +29,24 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.urhive.panicbutton.R;
+import com.urhive.panicbutton.helpers.Place_JSON;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 
@@ -49,7 +62,7 @@ public class NearByHospitalFragment extends FragmentBase implements OnMapReadyCa
                 .google.android.gms.location.LocationListener {
 
     private static final String TAG = NearByHospitalFragment.class.getSimpleName();
-    private static final int DEFAULT_ZOOM = 15;
+    private static final int DEFAULT_ZOOM = 12;
     private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
     private static double distanceInMeters;
     private static View view;
@@ -227,6 +240,10 @@ public class NearByHospitalFragment extends FragmentBase implements OnMapReadyCa
                 Log.d(TAG, "Current location is null. Using defaults.");
                 map.moveCamera(CameraUpdateFactory.newLatLngZoom(mDefaultLocation, DEFAULT_ZOOM));
             }
+
+            StringBuilder sbValue = new StringBuilder(sbMethod());
+            PlacesTask placesTask = new PlacesTask();
+            placesTask.execute(sbValue.toString());
             firstTimeCenter = false;
         }
     }
@@ -294,26 +311,11 @@ public class NearByHospitalFragment extends FragmentBase implements OnMapReadyCa
     }
 
     protected void stopLocationUpdates() {
-        LocationServices.FusedLocationApi.removeLocationUpdates(googleApiClient, this);
-    }
+        try {
+            LocationServices.FusedLocationApi.removeLocationUpdates(googleApiClient, this);
+        } catch (Exception ignored) {
 
-    public StringBuilder sbMethod() {
-
-        //use your current location here
-        double mLatitude = 37.77657;
-        double mLongitude = -122.417506;
-
-        StringBuilder sb = new StringBuilder("https://maps.googleapis" + "" + "" +
-                ".com/maps/api/place/nearbysearch/json?");
-        sb.append("location=" + mLatitude + "," + mLongitude);
-        sb.append("&radius=5000");
-        sb.append("&types=" + "hospital");
-        sb.append("&sensor=true");
-        sb.append("&key=" + getString(R.string.google_maps_key));
-
-        Log.d("Map", "api: " + sb.toString());
-
-        return sb;
+        }
     }
 
     private void updateTrack() {
@@ -332,5 +334,157 @@ public class NearByHospitalFragment extends FragmentBase implements OnMapReadyCa
         }
         Snackbar.make(mainLL, "distance is: " + distanceInMeters, Snackbar.LENGTH_SHORT).show();
         gpsTrack.setPoints(points);
+    }
+
+    public StringBuilder sbMethod() {
+        //use your current location here
+        double mLatitude = lastKnownLatLng.latitude;
+        double mLongitude = lastKnownLatLng.longitude;
+
+        StringBuilder sb = new StringBuilder("https://maps.googleapis" + "" + "" + "" +
+                ".com/maps/api/place/nearbysearch/json?");
+        sb.append("location=" + mLatitude + "," + mLongitude);
+        sb.append("&radius=5000");
+        sb.append("&types=" + "hospital");
+        sb.append("&sensor=true");
+        sb.append("&key=" + getString(R.string.pb_google_api_key));
+
+        Log.d("Map", "api: " + sb.toString());
+
+        return sb;
+    }
+
+    private String downloadUrl(String strUrl) throws IOException {
+        String data = "";
+        InputStream iStream = null;
+        HttpURLConnection urlConnection = null;
+        try {
+            URL url = new URL(strUrl);
+
+            // Creating an http connection to communicate with url
+            urlConnection = (HttpURLConnection) url.openConnection();
+
+            // Connecting to url
+            urlConnection.connect();
+
+            // Reading data from url
+            iStream = urlConnection.getInputStream();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(iStream));
+
+            StringBuffer sb = new StringBuffer();
+
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+
+            data = sb.toString();
+
+            br.close();
+
+        } catch (Exception e) {
+            Log.d(TAG, "Exception while downloading url" + e.toString());
+        } finally {
+            iStream.close();
+            urlConnection.disconnect();
+        }
+        return data;
+    }
+
+    private class PlacesTask extends AsyncTask<String, Integer, String> {
+
+        String data = null;
+
+        // Invoked by execute() method of this object
+        @Override
+        protected String doInBackground(String... url) {
+            try {
+                data = downloadUrl(url[0]);
+            } catch (Exception e) {
+                Log.d("Background Task", e.toString());
+            }
+            return data;
+        }
+
+        // Executed after the complete execution of doInBackground() method
+        @Override
+        protected void onPostExecute(String result) {
+            ParserTask parserTask = new ParserTask();
+
+            // Start parsing the Google places in JSON format
+            // Invokes the "doInBackground()" method of the class ParserTask
+            parserTask.execute(result);
+        }
+    }
+
+    private class ParserTask extends AsyncTask<String, Integer, List<HashMap<String, String>>> {
+
+        JSONObject jObject;
+
+        // Invoked by execute() method of this object
+        @Override
+        protected List<HashMap<String, String>> doInBackground(String... jsonData) {
+
+            List<HashMap<String, String>> places = null;
+            Place_JSON placeJson = new Place_JSON();
+
+            try {
+                jObject = new JSONObject(jsonData[0]);
+
+                places = placeJson.parse(jObject);
+
+            } catch (Exception e) {
+                Log.d("Exception", e.toString());
+            }
+            return places;
+        }
+
+        // Executed after the complete execution of doInBackground() method
+        @Override
+        protected void onPostExecute(List<HashMap<String, String>> list) {
+
+            Log.d("Map", "list size: " + list.size());
+            // Clears all the existing markers;
+            map.clear();
+
+            for (int i = 0; i < list.size(); i++) {
+
+                // Creating a marker
+                MarkerOptions markerOptions = new MarkerOptions();
+
+                // Getting a place from the places list
+                HashMap<String, String> hmPlace = list.get(i);
+
+
+                // Getting latitude of the place
+                double lat = Double.parseDouble(hmPlace.get("lat"));
+
+                // Getting longitude of the place
+                double lng = Double.parseDouble(hmPlace.get("lng"));
+
+                // Getting name
+                String name = hmPlace.get("place_name");
+
+                Log.d("Map", "place: " + name);
+
+                // Getting vicinity
+                String vicinity = hmPlace.get("vicinity");
+
+                LatLng latLng = new LatLng(lat, lng);
+
+                // Setting the position for the marker
+                markerOptions.position(latLng);
+
+                markerOptions.title(name + " : " + vicinity);
+
+                markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory
+                        .HUE_MAGENTA));
+
+                // Placing a marker on the touched position
+                Marker m = map.addMarker(markerOptions);
+
+            }
+        }
     }
 }
